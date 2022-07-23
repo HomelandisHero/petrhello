d81bcf4ad0a6081eef0990b4f6ec85400c766c0a
Serhii <yevstratovserhii@gmail.com>
2022-07-23 14:30:11 +0100

    add Cl.20/Object

--- js/script.js
+++ js/script.js
@@ -167,6 +167,7 @@ const test = "12.2px";
 console.log(parseFloat(test));
 
 // ---- CLASS 19 CALLBACK FUNCTIONS ----
+
 function first() {
         //Do something
         setTimeout(function() {
@@ -191,3 +192,45 @@ function done() {
 }
 
 learnJS('JavaScript', done);
+
+// ---- CLASS 20 "OBJECT" ----
+
+const options = {
+        name: 'test',
+        width: 1024,
+        height: 1024,
+        colors: {
+                border: 'black',
+                bg: 'red'
+        }, 
+        makeTest: function() {
+                console.log("Test");
+        }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//         if (typeof(options[key]) ==='object') {
+//                 for (let i in options[key]) {
+//                         console.log(`Властивість ${i} має значення ${options[key][i]}`);
+                        
+//                 }
+//         } else {
+//                 console.log(`Властивість ${key} має значення ${options[key]}`);
+//                 counter++;
+//         }
+        
+// }
+// console.log(counter);
