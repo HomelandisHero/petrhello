e09aa5051ff957d40215bbc0f670cf9c7e8c6063
Serhii <yevstratovserhii@gmail.com>
2022-07-23 12:46:36 +0100

    add functions genres/cl.18/pr.p.3

--- js/015 script.js
+++ js/015 script.js
@@ -14,7 +14,17 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+    }
+
+}
+start(); 
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -25,29 +35,53 @@ const personalMovieDB = {
 
 };
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один з переглянутих фільмів', ''),
-//           b = prompt('Як ви його оцінете?', '');
 
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('ERROR');
-//         i--;
-//     }
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один з переглянутих фільмів', ''),
+              b = prompt('Як ви його оцінете?', '');
     
-// }
-
-if (personalMovieDB.count < 10) {
-    console.log("Not much films have been watch");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("You are greate viewer");
-} else if (personalMovieDB.count >= 30) {
-    console.log("You are Filmkiller");
-} else { 
-    console.log("ERROR");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('ERROR');
+            i--;
+        }
+    
+        
+    }
 }
 
-console.log(personalMovieDB);
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Not much films have been watch");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("You are greate viewer");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You are Filmkiller");
+    } else { 
+        console.log("ERROR");
+    }
+}
+
+// detectPersonalLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat); 
+
+function writeYourGenres() {
+    for (let i = 1; i <=3; i++) {
+        personalMovieDB.genres[i - 1] =  prompt(`Ваш улюблений жанр під номером ${i}`);
+
+    }
+}writeYourGenres();
--- js/script.js
+++ js/script.js
@@ -163,5 +163,5 @@ const num = 12.2;
 console.log(Math.round(num));
 
 const test = "12.2px";
-console.log(parseInt(test));
+// console.log(parseInt(test));
 console.log(parseFloat(test));
